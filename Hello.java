f9b338bed29f1874eb202455dc248edbd7c07982
bharathireddicherla <68938765+bharathireddicherla@users.noreply.github.com>
2021-09-28 14:52:26 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello{
 	public static void main(String args[]){
 		for (int i=1;i<=10;i++){
 			System.out.println("Hello World....."+i);
-			System.out.println("Reddicherla Bharathi HCL");
+			System.out.println("Reddicherla Bharathi HCL company");
 			System.out.println("Thanks for the webhooks demo36..");
 			System.out.println("Thanks for the webhooks demonstration..");
 			System.out.println("Thanks for the webhooks demonstration 2..");
